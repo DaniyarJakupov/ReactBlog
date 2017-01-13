796fc8ea99d5d7c291723f39f66a41efbf1a850b
Daniyar <ddr199305@gmail.com>
2017-01-13 13:57:31 +0100

    Upgrade PostsIndex from functional to class based component. Connect it to app state. Create method for calling action creator

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'; // to make Container from Component
+import {fetchPosts} from '../actions/index';
 
-export default () => {
-   return <div>List of posts</div>
-};
+// Class based Component (Container)
+class  PostsIndex extends  Component {
+   // Life cycle method that React will call automatically
+   // whenever component is about to be rendered for the first time
+   componentWillMount() {
+      console.log("time to call action creator to fetch posts");
+      this.props.fetchPosts();
+   }
+
+   render(){
+      return (
+         <div>
+            List of posts
+         </div>
+      )
+   }
+}
+
+// Take component PostsIndex  take function mapStateToProps()
+// and take function mapDispatchToProps() then
+//  promote PostsIndex component to Container  (component that has state).
+// Make return values of above functions (e.g. fetchPosts) available
+// as a props in PostsIndex class.
+export default  connect(null, { fetchPosts })(PostsIndex);
