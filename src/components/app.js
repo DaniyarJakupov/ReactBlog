4821c431a07e7e3ec59ac6550114cc2f812f9457
Daniyar <ddr199305@gmail.com>
2017-01-14 08:55:14 +0100

    Add menu to App Component

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-         React simple starter
+         <div className="ui secondary pointing menu">
+            <div className="ui container">
+               <div className="header item">
+                  <i className="write icon"></i> Blog
+               </div>
+               <Link to="/" className="link item">
+                  Home
+               </Link>
+            </div>
+         </div>
          {/* render nested routes */}
          {this.props.children}
       </div>
