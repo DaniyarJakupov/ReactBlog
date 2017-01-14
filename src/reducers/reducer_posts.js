b323cd959652bba978cccd192aab93c02172de53
Daniyar <ddr199305@gmail.com>
2017-01-14 13:35:59 +0100

    Delete console.log

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -8,7 +8,6 @@ import { FETCH_POSTS } from '../actions/index';
 const INITIAL_STATE = { all: [], post: null};
 
 export default function(state = INITIAL_STATE, action){
-   console.log("reducer_posts: ", action);
    switch(action.type){
    case FETCH_POSTS:
       return { ...state, all: action.payload.data };
